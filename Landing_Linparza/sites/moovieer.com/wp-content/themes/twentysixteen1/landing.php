bc3e2749356f93b3c1dab89ad86d96ed5633fcab
Lina <angelo.9720@gmail.com>
2020-06-19 21:23:49 +0300

    Add files via upload

--- Landing_Linparza/sites/moovieer.com/wp-content/themes/twentysixteen1/landing.php
+++ Landing_Linparza/sites/moovieer.com/wp-content/themes/twentysixteen1/landing.php
@@ -18,262 +18,45 @@
 
 <body class="">
 <!-- View - 1 Block -->
-<section class="view">
+<section class="view-main">
     <!-- Header -->
-    <div class="bg__view"></div>
+    <div class="bg__view-main"></div>
 		<header class="container header">
 			<div class="header__logo">
 				<img src="https://moovieer.com/wp-content/uploads/logo.png" alt="logo">
 			</div>
+<!--            <div class="view__btn_open mob-none"><a href="">Вход в зрительный зал</a></div>-->
 			<div class="header__logo">
 				<img class="header__left" src="https://moovieer.com/wp-content/uploads/logo_2.png" alt="logo_2">
 			</div>
 		</header>
-		<h1 class="container view__title">
-			2020 – год прорыва в лечении <i>BRCA</i> - ассоциированных опухолей женской репродуктивной системы
-		</h1>
-		<div class="view__video"></div>
-		<div class="container view__video-btn">
-			<div class="view__btn_open"><a href="">Вход в зрительный зал</a></div>
-			<div class="view__btn_flex">
-				<div class="view__btn"><button class="make-photo">Сделать фото</button></div>
-				<div class="view__btn"><button class="load-photo">Загрузить фото</button></div>
-			</div>
-		</div>
+    <form action="" class="container view__form-registration">
+        <input type="text" class="view__input" placeholder="Имя">
+        <input type="text"class="view__input" placeholder="Фамилия">
+        <div class="view__checkbox-block first">
+            <label>Я подтверждаю, что являюсь специалистом здравоохранения.
+                <input type="checkbox" id="specialist" />
+                <span></span>
+            </label>
+        </div>
+        <div class="view__btn_center">
+            				<div class="view__btn-center"><button class="make-photo">Сделать фото</button></div>
+            				<div class="view__btn-center"><button class="load-photo">Загрузить фото</button></div>
+            			</div>
+        <div class="view__checkbox-block second">
+                <label>Согласен на использование своего изображения в рамках мероприятия. <a href="" class="agreement">Cоглашение</a>
+                    <input type="checkbox" id="agreement" />
+                    <span></span>
+                </label>
+        </div>
+        <div class="view__checkbox-block">
+            <label>Я согласен с обработкой данных, предоставленных мною выше.
+                <input type="checkbox" id="processing" />
+                <span></span>
+            </label>
+        </div>
+    </form>
 	</section>
-
-	<!-- Speaker - 2 Block -->
-	<section class="speaker">
-		<h1 class="speaker__title">СПИКЕРЫ</h1>
-		<div class="speaker__slider">
-			<div class="speaker__item">
-				<img src="https://moovieer.com/wp-content/uploads/Nechushkina.png" alt="Nechushkina">
-				<div class="speaker__text">
-					<div class="speaker__text-title">Нечушкина Валентина Михайловна
-					</div>
-					<div class="speaker__text-subtext">Д.м.н., Заместитель директора Института онкогинекологии и
-						маммологии
-						ФГБУ «НМИЦ акушерства, гинекологии
-						и перинатологии им. В.И. Кулакова Министерства здравоохранения Российской Федерации»</div>
-				</div>
-			</div>
-			<div class="speaker__item">
-				<img src="https://moovieer.com/wp-content/uploads/Demidova.png" alt="Demidova">
-				<div class="speaker__text">
-					<div class="speaker__text-title">Демидова Ирина Анатольевна
-					</div>
-					<div class="speaker__text-subtext">К.м.н., заведующая молекулярно-биологической лабораторией
-						ГБУЗ<br>
-						«МГОБ 62 ДЗМ», Москва</div>
-				</div>
-			</div>
-			<div class="speaker__item">
-				<img src="https://moovieer.com/wp-content/uploads/Nechushkina.png" alt="Nechushkina">
-				<div class="speaker__text">
-					<div class="speaker__text-title">Нечушкина Валентина Михайловна</div>
-					<div class="speaker__text-subtext">Д.м.н., Заместитель директора Института онкогинекологии и
-						маммологии
-						ФГБУ «НМИЦ акушерства, гинекологии
-						и перинатологии им. В.И. Кулакова Министерства здравоохранения Российской Федерации»</div>
-				</div>
-			</div>
-			<div class="speaker__item">
-				<img src="https://moovieer.com/wp-content/uploads/Demidova.png" alt="Demidova">
-				<div class="speaker__text">
-					<div class="speaker__text-title">Демидова Ирина Анатольевна
-					</div>
-					<div class="speaker__text-subtext">К.м.н., заведующая молекулярно-биологической лабораторией
-						ГБУЗ<br>
-						«МГОБ 62 ДЗМ», Москва</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<!-- Events - 3 Block -->
-	<section class="container events">
-		<h1 class="events__title">ПОСМОТРЕТЬ ПРОШЕДШИЕ МЕРОПРИЯТИЯ</h1>
-		<div class="events__slider">
-			<div class="events__item">
-				<img src="https://moovieer.com/wp-content/uploads/video_1.png" alt="video_1">
-				<div class="events__info">Запись меропрятия 30.06</div>
-			</div>
-			<div class="events__item">
-				<img src="https://moovieer.com/wp-content/uploads/video_2.png" alt="video_2">
-				<div class="events__info">Запись меропрятия 7.07</div>
-			</div>
-			<div class="events__item">
-				<img src="https://moovieer.com/wp-content/uploads/video_1.png" alt="video_1">
-				<div class="events__info">Запись меропрятия 30.06</div>
-			</div>
-			<div class="events__item">
-				<img src="https://moovieer.com/wp-content/uploads/video_2.png" alt="video_2">
-				<div class="events__info">Запись меропрятия 7.07</div>
-			</div>
-		</div>
-	</section>
-
-	<a href="" class="information-connection">Техническая информация по подключению</a>
-
-	<!-- Description - 4 Block -->
-	<section class="description">
-        <div class="bg__description"></div>
-		<h1 class="description__title">Линпарза®</h1>
-		<div class="description__about">
-			<div class="description__text">– единственный PARP-ингибитор, одобренный
-				в России к применению в качестве поддерживающей
-				монотерапии ВПЕРВЫЕ выявленного распространенного
-				рака яичников с мутацией в генах <i>BRCA</i> 1/2 c ответом
-				на ПЕРВУЮ ЛИНИЮ химиотерапии1.</div>
-			<div class="description__text margin">– первый зарегистрированный в России ингибитор
-				PARP, предлагающий альтернативу химиотерапии
-				пациентам с метастатическим HER2-негативным
-				раком молочной железы с герминальной мутацией
-				в генах <i>BRCA</i> 1/21.</div>
-			<div class="description__instruction">
-				<div class="description__color">1.</div> Инструкция по медицинскому применению лекарственного
-				препарата Линпарза® (таблетки, покрытые пленочной оболочкой,
-				10 и 150 мг). Регистрационное удостоверение ЛП-009941 от 26.11.2019.
-				Доступно по <a href="" class="href__green">ссылке</a>. (дата обращения 07.06.2020).
-			</div>
-		</div>
-	</section>
-
-	<!-- Contacts - 5 Block -->
-	<section class="contacts">
-		<div class="contacts__block">
-			<h1 class="contacts__title">Контакты службы технической поддержки</h1>
-			<div class="contacts__phone">
-				<a href="tel:8(495)123-45-67">8 (495) 123-45-67</a>
-				<a href="tel:8(495)123-45-67">8 (495) 123-45-67</a>
-			</div>
-			<div class="contacts__text">Материал предназначен для специалистов здравоохранения. Имеются
-				противопоказания.<br>
-				Перед применением ознакомьтесь, пожалуйста, с полной инструкцией по медицинскому применению
-				лекарственного
-				препарата, перейдя по ссылке. Если Вам стало известно
-				о нежелательной реакции при использовании лекарственного препарата «АстраЗенека», пожалуйста, сообщите
-				эту
-				информацию в медицинский отдел компании. Вы можете написать
-				нам по электронной почте <a href="mailto:Safety.Russia@astrazeneca.com"
-					class="href__green">Safety.Russia@astrazeneca.com</a> , заполнить веб-форму<br> <a
-					href="https://aereporting.astrazeneca.com"
-					class="href__green">https://aereporting.astrazeneca.com</a> или связаться с нами по телефону 8 (495)
-				799-56-99, доб.2580 </div>
-		</div>
-	</section>
-
-	<!-- Footer -->
-	<footer class="footer">
-		<div class="footer__img">
-			<img src="https://moovieer.com/wp-content/uploads/logo_3.png" alt="logo_3">
-		</div>
-		<div class="footer__info">ООО «АстраЗенека Фармасьютикалз»<br>
-			Россия 123112, Москва, 1-й Красногвардейский проезд, д.21 стр. 1<br>
-			Тел.: +7 (495) 799 56 99, факс: +7 (495) 799 56 98<br>
-			<a href="www.astrazeneca.ru" class="href__green">www.astrazeneca.ru</a>
-		</div>
-	</footer>
-
-
-	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-	<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
-	<script type="text/javascript">
-		$(document).ready(function () {
-			$('.speaker__slider').slick({
-				infinite: true,
-  				speed: 300,
-				prevArrow: '<div class="prev speaker"><img src="https://moovieer.com/wp-content/uploads/arrow_left.png" alt=""></div>',
-				nextArrow: '<div class="next speaker"><img src="https://moovieer.com/wp-content/uploads/arrow_right.png" alt=""></div>',
-				// slidesToShow: 2,
-				// slidesToScroll: 2,
-				responsive: [
-				{
-						breakpoint: 3440,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-				{
-						breakpoint: 1980,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-				{
-						breakpoint: 1440,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
-					},
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
-					},{
-						breakpoint: 480,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-				]
-			});
-		});
-
-		$(document).ready(function () {
-			$('.events__slider').slick({
-				infinite: true,
-				// slidesToShow: 2,
-				// slidesToScroll: 2,
-				prevArrow: '<div class="prev events"><img src="https://moovieer.com/wp-content/uploads/arrow_left.png" alt=""></div>',
-				nextArrow: '<div class="next events"><img src="https://moovieer.com/wp-content/uploads/arrow_right.png" alt=""></div>',
-				responsive: [
-				{
-						breakpoint: 3440,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-				{
-						breakpoint: 1980,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-				{
-						breakpoint: 1440,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
-					},
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
-					},{
-						breakpoint: 480,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-				]
-			});
-		});
-	</script>
 
 </body>
 
